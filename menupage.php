dfdd0d060c4d125eee59fde398223214b24147c3
Titus <titus@mailbox.org>
2020-03-30 22:28:52 +0100

    do not hardcode db prefix

--- menupage.php
+++ menupage.php
@@ -8,12 +8,12 @@ function onk2019_menupage () {
 	global $wpdb;
 	$startstop = $wpdb->get_results(
 		'SELECT MIN(date) as mindate, MAX(date) as maxdate ' .
-		'FROM `uLZvWUiRonk2019_counter`'
+		'FROM `' . $wpdb->prefix . 'onk2019_counter`'
 	)[0];
 	$topevents = $wpdb->get_results(
 		'SELECT event, organiser, name, count(*) as clicks ' .
-		'FROM `uLZvWUiRonk2019_counter` ct ' .
-                'LEFT JOIN `uLZvWUiRonk2019` ev ' .
+		'FROM `' . $wpdb->prefix . 'onk2019_counter` ct ' .
+                'LEFT JOIN `' . $wpdb->prefix . 'onk2019` ev ' .
 		'ON ct.event = ev.id ' .
 		'GROUP BY event ORDER BY clicks DESC LIMIT 20'
 	);
